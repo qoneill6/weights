b1877cb9bcf8ebca179b62f257630c2de871df45
qoneill6 <qoneill@stevens.edu>
2026-08-03 23:08:10 -0400

    Update self_weight_project.py
    this is the addition of the interface to be run threw the cmd

--- self_weight_project.py
+++ self_weight_project.py
@@ -1,9 +1,14 @@
 #gym math
 import streamlit as st
+
+choice = st.radio("Equitment:", ["Barbell","Machine"])
+weight= b if choice=="Barbell" else m 
+
+
 m= [45,25,10,5,2.5]
 b = [90,50,20,10,5]
 def find_m (insert,weight,c):
-    round2(insert)
+    insert = round2(insert)
     if insert==0 or weight ==[]:
         return c  
     elif insert >= weight [0]:
@@ -22,7 +27,7 @@ def run (insert,weight,c):
     else:
         return run(insert,weight[1:],c)
 def find_b(insert,weight,c):
-    round5(insert)
+    insert=round5(insert)
     if insert > 45:
         insert = insert - 45
         return run(insert,weight,c)
@@ -61,8 +66,8 @@ def changeb(old,new):
     if old==new : hold = "1"
     if old>new: hold = "2"
     if new>old: hold = "3"
-    if old>new and (old or new )%5 !=0: hold="4"
-    if new>old and (old or new )%5 !=0: hold="5"
+    if old>new and (old% 5 !=0 or new%5 !=0): hold="4"
+    if new>old and (old% 5 !=0 or new%5 !=0): hold="5"
     match hold :
         case "1":  #same
             return f"The same weight{calc(new,b)}" 
@@ -82,8 +87,8 @@ def changem(old,new):
     if old==new : hold = "1"
     if old>new: hold = "2"
     if new>old: hold = "3"
-    if old>new and (old or new )%2.5 !=0: hold="4"
-    if new>old and (old or new )%2.5 !=0: hold="5"
+    if old>new and (old% 2.5 !=0 or new%2.5 !=0): hold="4"
+    if new>old and (old% 2.5 !=0 or new%2.5 !=0): hold="5"
     match hold :
         case "1":  #same
             return f"The same weight{calc(new,m)}" 
@@ -101,56 +106,38 @@ def changem(old,new):
             new = round2(new)
             
             return f"Add {new-old}lb to the machine should now look like {calc(new,m)}(rounded up)"
-def p():
-    c=[]
-    ma=int(input("what is you max:"))
-    count = int(input("how many sets :") )
-    def pwork(count):
-        if count == 0:
-            return c
-        elif count > 8:
-            return "do less sets and more weight:"
-        else:
-            h= int(input("what percetage:"))
-            h=(h/100)*ma
-            c.append(h)
-            return pwork(count-1)
-    re=pwork(count)
-    return re
+        
+if "sets" not in st.seasion_state:
+    st.session_state.sets =[]
+ma= st.number_input("what is you max:",min_value=0, step=1)
+count = st.number_input("how many sets :",min_value=0 , max_value=6, step=1)
+percentges=[]
 
+for i in range(count):
+    pct= st.number_input(f"Percent of set {i+1} : ", min_value=0,max_value=100)
+    percentges.append(pct)
+if st.button("calculate:"):
+    st.session_state.sets=[p/100*ma for p in percentages]
+
+    
 def changes(x):
-    g=p()
+    g=st.seasion_state
+    if not g:
+        return
     if x==b:
-        print (f"Start by putting {calc(g[0],b)} on the bar")
+        st.write(f"Start by putting {calc(g[0],b)} on the bar")
         for i in range(1,len(g)):
-            print (changeb(g[i-1],g[i]))
+            st.write (changeb(g[i-1],g[i]))
     else : #x==m
-        print (f"Start by putting {calc(g[0],m)} on the machine")
+        st.write(f"Start by putting {calc(g[0],m)} on the machine")
         for i in range(1,len(g)):
-            print (changem(g[i-1],g[i]))
-
-# this is where i am starting to write interface 
-
-
+            st.write(changem(g[i-1],g[i]))
+    
+                
 
 
+if st.button("Show my weights"):
+    changes(weight)
     
         
             
-
-
-
-
-        
-        
-        
-        
-
-    
-
-
-
-   
-        
-
-        
